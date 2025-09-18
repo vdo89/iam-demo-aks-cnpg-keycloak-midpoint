005e7fe834e1447962903e2fc5afd4ea8362eb4d
vdo89 <vleerdoing@gmail.com>
2025-09-18 19:01:22 +0200

    Clarify AKS sizing comment

--- infra/azure/terraform/variables.tf
+++ infra/azure/terraform/variables.tf
@@ -9,3 +9,15 @@ variable "prefix" {
   description = "Resource prefix (short, lowercase)"
   default     = "rwsdemo"
 }
+
+variable "aks_default_node_vm_size" {
+  type        = string
+  description = "VM size for the default AKS node pool"
+  default     = "Standard_D4s_v3"
+}
+
+variable "aks_default_node_count" {
+  type        = number
+  description = "Number of nodes in the default AKS node pool"
+  default     = 3
+}
